dbac75b3ce3cb341931d9c8157ef3eadeea22cfd
Zach Smith <zsmith126028@gmail.com>
2019-05-27 17:18:14 -0500

    reformat layout

--- src/components/BaseballCard.js
+++ src/components/BaseballCard.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import './BaseballCard.css';
 
 class BaseballCard extends Component {
 
   render() {
     return (
       <div className="Baseball-card">
-        <h2 className="Baseball-card-title">{this.props.name}</h2>
-        <img src={this.props.img} alt=""/>
-        <div className="Baseball-card-stats">
-          <p>Games: {this.props.games}</p>
-          <p>Plate Appearances: {this.props.pa}</p>
-          <p>Homeruns: {this.props.hr}</p>
-          <p>Runs: {this.props.runs}</p>
-          <p>RBI: {this.props.rbi}</p>
-          <p>SB: {this.props.sb}</p>
-          <p>Slashline: {this.props.avg}/{this.props.obp}/{this.props.slg}</p>
+        <div className="Baseball-card-inner">
+          <div className="Baseball-card-front">
+            <img className="Baseball-card-image" src={this.props.img} alt=""/>
+          </div>
+          <div className="Baseball-card-back">
+            <h1 className="Baseball-card-title">{this.props.name}</h1>
+            <p>Games: {this.props.games}</p>
+            <p>Plate Appearances: {this.props.pa}</p>
+            <p>Homeruns: {this.props.hr}</p>
+            <p>Runs: {this.props.runs}</p>
+            <p>RBI: {this.props.rbi}</p>
+            <p>SB: {this.props.sb}</p>
+            <p>Slashline: {this.props.avg}/{this.props.obp}/{this.props.slg}</p>
+          </div>
         </div>
       </div>
     )
@@ -22,5 +27,3 @@ class BaseballCard extends Component {
 }
 
 export default BaseballCard;
-
-// https://securea.mlb.com/mlb/images/players/head_shot/595879.jpg
